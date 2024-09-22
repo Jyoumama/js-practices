9fd1a71388e7fc218bc26d3734fa7bc994968b23
jyouma <juri19771211@gmail.com>
2024-09-22 09:00:52 +0900

    promise.jsを実装した

---
+++ 03.asynchronous/promise.js
@@ -0,0 +1,32 @@
+import { runAsync, allAsync, closeAsync } from "./promise-utils.js";
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database(":memory:");
+
+runAsync(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => {
+    console.log("Table created");
+    return runAsync(db, "INSERT INTO books (title) VALUES (?)", ["Book 1"]);
+  })
+  .then((result) => {
+    console.log("Inserted record with ID:", result.lastID);
+    return runAsync(db, "INSERT INTO books (title) VALUES (?)", ["Book 2"]);
+  })
+  .then((result) => {
+    console.log("Inserted record with ID:", result.lastID);
+    return allAsync(db, "SELECT * FROM books");
+  })
+  .then((rows) => {
+    console.log("Records:", rows);
+    return runAsync(db, "DROP TABLE books");
+  })
+  .then(() => {
+    console.log("Table deleted");
+    return closeAsync(db);
+  })
+  .then(() => {
+    console.log("Database closed");
+  });
