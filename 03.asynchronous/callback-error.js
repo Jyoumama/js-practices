e13ab6e44108d6754c50569f2a21c0e43f8b85fa
jyouma <juri19771211@gmail.com>
2024-10-07 12:47:35 +0900

    callback-error.jsのthis未参照部分をアロー関数に修正

--- 03.asynchronous/callback-error.js
+++ 03.asynchronous/callback-error.js
@@ -1,56 +1,37 @@
 import sqlite3 from "sqlite3";
+
 const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  (err) => {
-    if (err) {
-      console.error("Error creating table:", err.message);
-      return;
-    }
+  () => {
     console.log("Table created");
 
-    db.run("INSERT INTO books (title) VALUES (?)", ["Book 1"], function (err) {
-      if (err) {
-        console.error("Error inserting record:", err.message);
-        return;
-      }
+    db.run("INSERT INTO books (title) VALUES (?)", ["Book 1"], function () {
       console.log("Inserted record with ID:", this.lastID);
 
-      db.run(
-        "INSERT INTO books (title) VALUES (?)",
-        ["Book 1"],
-        function (err) {
-          if (err) {
-            console.error("Error inserting duplicate record:", err.message);
+      db.run("INSERT INTO books (title) VALUES (?)", ["Book 1"], (error) => {
+        if (error) {
+          console.error("Error inserting duplicate record:", error.message);
+        }
+
+        db.all("SELECT * FROM non_existent_table", (error) => {
+          if (error) {
+            console.error(
+              "Error fetching from non-existent table:",
+              error.message,
+            );
           }
 
-          db.all("SELECT * FROM non_existent_table", (err) => {
-            if (err) {
-              console.error(
-                "Error fetching from non-existent table:",
-                err.message,
-              );
-            }
+          db.run("DROP TABLE books", () => {
+            console.log("Table deleted");
 
-            db.run("DROP TABLE books", (err) => {
-              if (err) {
-                console.error("Error deleting table:", err.message);
-                return;
-              }
-              console.log("Table deleted");
-
-              db.close((err) => {
-                if (err) {
-                  console.error("Error closing database:", err.message);
-                  return;
-                }
-                console.log("Database closed");
-              });
+            db.close(() => {
+              console.log("Database closed");
             });
           });
-        },
-      );
+        });
+      });
     });
   },
 );
