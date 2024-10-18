5f4ad089e2fb258c6638c2c79b8da173faa7844b
jyouma <juri19771211@gmail.com>
2024-10-18 16:00:45 +0900

    async-await-error.jsの予期しないエラーを最上位の catch ブロックでキャッチされるように修正

--- 03.asynchronous/async-await-error.js
+++ 03.asynchronous/async-await-error.js
@@ -3,47 +3,57 @@ import { runAsync, getAsync, closeAsync } from "./promise-shared.js";
 
 const db = new sqlite3.Database(":memory:");
 
-await runAsync(
-  db,
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-);
-console.log("Table created");
+async function myDatabaseOperations() {
+  await runAsync(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+  console.log("Table created");
 
-const result1 = await runAsync(db, "INSERT INTO books (title) VALUES (?)", [
-  "Book 1",
-]);
-console.log("Inserted record with ID:", result1.lastID);
+  const result1 = await runAsync(db, "INSERT INTO books (title) VALUES (?)", [
+    "Book 1",
+  ]);
+  console.log("Inserted record with ID:", result1.lastID);
 
-try {
-  await runAsync(db, "INSERT INTO books (title) VALUES (?)", ["Book 1"]);
-} catch (error) {
-  if (error && "code" in error) {
-    if (error.code === "SQLITE_CONSTRAINT") {
-      console.error("Error inserting duplicate record:", error.message);
+  try {
+    await runAsync(db, "INSERT INTO books (title) VALUES (?)", ["Book 1"]);
+  } catch (error) {
+    if (error && "code" in error) {
+      if (error.code === "SQLITE_CONSTRAINT") {
+        console.error("Error inserting duplicate record:", error.message);
+      } else {
+        throw error;
+      }
     } else {
       throw error;
     }
-  } else {
-    console.error("Unexpected error format:", error);
   }
-}
 
-try {
-  await getAsync(db, "SELECT * FROM non_existent_table");
-} catch (error) {
-  if (error && "message" in error) {
-    if (error.message.includes("no such table")) {
-      console.error("Error fetching from non-existent table:", error.message);
+  try {
+    await getAsync(db, "SELECT * FROM non_existent_table");
+  } catch (error) {
+    if (error && "message" in error) {
+      if (error.message.includes("no such table")) {
+        console.error("Error fetching from non-existent table:", error.message);
+      } else {
+        throw error;
+      }
     } else {
       throw error;
     }
-  } else {
-    console.error("Unexpected error format:", error);
   }
+
+  await runAsync(db, "DROP TABLE books");
+  console.log("Table deleted");
+
+  await closeAsync(db);
+  console.log("Database closed");
 }
 
-await runAsync(db, "DROP TABLE books");
-console.log("Table deleted");
-
-await closeAsync(db);
-console.log("Database closed");
+(async () => {
+  try {
+    await myDatabaseOperations();
+  } catch (error) {
+    console.error("An unexpected error occured:", error.message);
+  }
+})();
