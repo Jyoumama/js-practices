353cacc77538bc01a0ccc6f26bb432c9c634c843
jyouma <juri19771211@gmail.com>
2024-10-19 06:15:03 +0900

    async-await-error.jsのimport名をsqlite-async.jsに変更

--- 03.asynchronous/async-await-error.js
+++ 03.asynchronous/async-await-error.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { runAsync, getAsync, closeAsync } from "./promise-shared.js";
+import { runAsync, getAsync, closeAsync } from "./sqlite-async.js";
 
 const db = new sqlite3.Database(":memory:");
 
