30acd9c8cba221f91ac5d74612845dbd40e01b34
jyouma <juri19771211@gmail.com>
2024-10-23 01:09:03 +0900

    async-await-error.jsのエラーハンドリングにinstanceof ErrorでErrorオブジェクトの判定を加え想定外のエラーをunexpected errorとして再スローするように設定

--- 03.asynchronous/async-await-error.js
+++ 03.asynchronous/async-await-error.js
@@ -5,25 +5,22 @@ const db = new sqlite3.Database(":memory:");
 
 await runAsync(
   db,
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 );
 console.log("Table created");
 
-
-const result1 = await runAsync(db, "INSERT INTO books (title) VALUES (?)", ["Book 1"]);
+const result1 = await runAsync(db, "INSERT INTO books (title) VALUES (?)", [
+  "Book 1",
+]);
 console.log("Inserted record with ID:", result1.lastID);
 
 try {
   await runAsync(db, "INSERT INTO books (title) VALUES (?)", ["Book 1"]);
 } catch (error) {
-  if (
-    typeof error === "object" &&
-    error !== null &&
-    "code" in error &&
-    error.code === "SQLITE_CONSTRAINT"
-  ) {
+  if (error instanceof Error && error.code === "SQLITE_CONSTRAINT") {
     console.error("Error inserting duplicate record:", error.message);
   } else {
+    console.error("Unexpected error:", error);
     throw error;
   }
 }
@@ -31,14 +28,10 @@ try {
 try {
   await getAsync(db, "SELECT * FROM non_existent_table");
 } catch (error) {
-  if (
-    typeof error === "object" &&
-    error !== null &&
-    "message" in error &&
-    error.message.includes("no such table")
-  ) {
+  if (error instanceof Error && error.message.includes("no such table")) {
     console.error("Error fetching from non-existent table:", error.message);
   } else {
+    console.error("Unexpected error:", error);
     throw error;
   }
 }
