8d998e7976c5bebe74c5079a77a55348793f06f6
jyouma <juri19771211@gmail.com>
2024-10-23 01:10:15 +0900

    promise-error.jsもasync-await-error.js同様にinstanceof ErrorでErrorオブジェクトの判定を加え想定外のエラーをunexpected errorとして再スローするように設定

--- 03.asynchronous/promise-error.js
+++ 03.asynchronous/promise-error.js
@@ -16,26 +16,19 @@ runAsync(
     return runAsync(db, "INSERT INTO books (title) VALUES (?)", ["Book 1"]);
   })
   .catch((error) => {
-    if (
-      typeof error === "object" &&
-      error !== null &&
-      "code" in error &&
-      error.code === "SQLITE_CONSTRAINT"
-    ) {
+    if (error instanceof Error && error.code === "SQLITE_CONSTRAINT") {
       console.error("Error inserting duplicate record:", error.message);
     } else {
+      console.error("Unexpected error:", error);
       throw error;
     }
   })
   .then(() => getAsync(db, "SELECT * FROM non_existent_table"))
   .catch((error) => {
-    if (
-      typeof error === "object" &&
-      error !== null &&
-      error.message.includes("no such table")
-    ) {
+    if (error instanceof Error && error.message.includes("no such table")) {
       console.error("Error fetching from non-existent table:", error.message);
     } else {
+      console.error("Unexpected error:", error);
       throw error;
     }
   })
