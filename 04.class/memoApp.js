63b2ee34d982bbd38ddd042c6912b15ceb631197
jyouma <juri19771211@gmail.com>
2024-11-22 15:44:21 +0900

    memoApp.jsのrunメソッド内の例外エラー処理をオプショナルチェイニング（?.）を使用しプロパティアクセス時のエラーを回避するよう修正

--- 04.class/memoApp.js
+++ 04.class/memoApp.js
@@ -33,12 +33,7 @@ export default class MemoApp {
         return;
       }
     } catch (err) {
-      console.error(
-        "Critical error occurred:",
-        err && typeof err === "object" && "message" in err
-          ? err.message
-          : String(err),
-      );
+      console.error("Critical error occurred:", err?.message || String(err));
     }
   }
 
